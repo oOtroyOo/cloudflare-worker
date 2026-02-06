6224cf0f7be9b0368e8120d9c4d33406235eccb9
oOtroyOo <497790325@qq.com>
2026-02-06 16:29:00 +0800

    ES 语法

--- src/index.js
+++ src/index.js
@@ -11,117 +11,116 @@
 const re = /^(http[s]?:\/\/)?([\w-]+\.)+[\w-]+(:\d+)?/i
 const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36'
 
-const exclude = require('./exclude.json');
-
+import exclude from './exclude.json' assert { type: 'json' };
 /**
  * example
  * 	{HOST}/https://api.ip.sb/geoip/
  *	{HOST}/https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v2.4.3/Clash.Verge_2.4.3_x64-setup.exe
  */
-/**
- *
- * @param {Request} request
- * @param {Cloudflare.Env} env
- * @param {ExecutionContext} ctx
- * @returns
-*/
-async function handle(request, env, ctx) {
-    for (const key of exclude) {
-        try {
-            if (request.url.includes(key) || request.url.match(key)) {
-                return new Response(null, { status: 404 });
-            }
-        } catch (error) {
-
-        }
-    }
-    console.log(`env: ${JSON.stringify(env)}`);
-    let basePath = ""
-
-    if (env != undefined && env['BASE_PATH']) {
-        basePath = env['BASE_PATH']
-    }
-    console.log(request.url);
-
-    const reqUrl = new URL(request.url)
-
-    let urlBase = reqUrl.href.substring(reqUrl.origin.length)
-    if (urlBase.startsWith(basePath + "/")) {
-        urlBase = urlBase.substring(basePath.length + 1)
-    } else {
-        urlBase = urlBase.substring(1)
-    }
-
-    if (re.test(urlBase)) {
-        if (!urlBase.startsWith("http")) {
-            urlBase = `https://${urlBase}`;
-        }
-        const toUrl = new URL(urlBase)
-
-        const proxyRequest = new Request(toUrl, {
-            method: request.method,
-            body: request.body,
-            referrer: request.referrer
-        });
-        if (request.headers) {
-            console.log("原headers:")
-            for (const [key, value] of request.headers.entries()) {
-                console.log(`${key}: ${value}`);
-                proxyRequest.headers.set(key, value);
-            }
-
-            proxyRequest.headers.set('Host', toUrl.host)
-            if (!request.headers.has('Referer')) {
-                proxyRequest.headers.set('Referer', toUrl.origin + '/');
-            }
-
-            if (!request.headers.has('Origin')) {
-                proxyRequest.headers.set('Origin', toUrl.origin);
-            }
-            if (!request.headers.has('User-Agent')) {
-                proxyRequest.headers.set('User-Agent', userAgent)
-            }
-            if (!request.headers.has('accept')) {
-                proxyRequest.headers.set('accept', "*/*")
-            }
-            if (!request.headers.has('accept-encoding')) {
-                proxyRequest.headers.set('accept-encoding', "gzip, deflate, br, zstd")
-            }
-            if (!request.headers.has('accept-language')) {
-                proxyRequest.headers.set('accept-language', "zh-CN,zh;q=0.9,en;q=0.8")
-            }
-        }
-        console.log("转headers:")
-        for (const [key, value] of proxyRequest.headers.entries()) {
-            console.log(`${key}: ${value}`);
-        }
-
-        console.log(toUrl.toString())
-        try {
-            return fetch(proxyRequest);
-        } catch (error) {
-            console.log(error);
-
-            return new Response(error, {
-                status: 400,
-                headers: {
-                    'Content-Type': 'text/plain; charset=utf8'
-                }
-            });
-        }
-
-
-    }
-    // return new Response("Hello")
-    return new Response('未能匹配到转义URL', {
-        status: 400,
-        headers: {
-            'Content-Type': 'text/plain; charset=utf8'
-        }
-    });
-
-}
 
 export default {
-    fetch: handle
+    /**
+     *
+     * @param {Request} request
+     * @param {Cloudflare.Env} env
+     * @param {ExecutionContext} ctx
+     * @returns
+    */
+    async fetch(request, env, ctx) {
+        for (const key of exclude) {
+            try {
+                if (request.url.includes(key) || request.url.match(key)) {
+                    return new Response(null, { status: 404 });
+                }
+            } catch (error) {
+
+            }
+        }
+        console.log(`env: ${JSON.stringify(env)}`);
+        let basePath = ""
+
+        if (env != undefined && env['BASE_PATH']) {
+            basePath = env['BASE_PATH']
+        }
+        console.log(request.url);
+
+        const reqUrl = new URL(request.url)
+
+        let urlBase = reqUrl.href.substring(reqUrl.origin.length)
+        if (urlBase.startsWith(basePath + "/")) {
+            urlBase = urlBase.substring(basePath.length + 1)
+        } else {
+            urlBase = urlBase.substring(1)
+        }
+
+        if (re.test(urlBase)) {
+            if (!urlBase.startsWith("http")) {
+                urlBase = `https://${urlBase}`;
+            }
+            const toUrl = new URL(urlBase)
+
+            const proxyRequest = new Request(toUrl, {
+                method: request.method,
+                body: request.body,
+                referrer: request.referrer
+            });
+            if (request.headers) {
+                console.log("原headers:")
+                for (const [key, value] of request.headers.entries()) {
+                    console.log(`${key}: ${value}`);
+                    proxyRequest.headers.set(key, value);
+                }
+
+                proxyRequest.headers.set('Host', toUrl.host)
+                if (!request.headers.has('Referer')) {
+                    proxyRequest.headers.set('Referer', toUrl.origin + '/');
+                }
+
+                if (!request.headers.has('Origin')) {
+                    proxyRequest.headers.set('Origin', toUrl.origin);
+                }
+                if (!request.headers.has('User-Agent')) {
+                    proxyRequest.headers.set('User-Agent', userAgent)
+                }
+                if (!request.headers.has('accept')) {
+                    proxyRequest.headers.set('accept', "*/*")
+                }
+                if (!request.headers.has('accept-encoding')) {
+                    proxyRequest.headers.set('accept-encoding', "gzip, deflate, br, zstd")
+                }
+                if (!request.headers.has('accept-language')) {
+                    proxyRequest.headers.set('accept-language', "zh-CN,zh;q=0.9,en;q=0.8")
+                }
+            }
+            console.log("转headers:")
+            for (const [key, value] of proxyRequest.headers.entries()) {
+                console.log(`${key}: ${value}`);
+            }
+
+            console.log(toUrl.toString())
+            try {
+                return fetch(proxyRequest);
+            } catch (error) {
+                console.log(error);
+
+                return new Response(error, {
+                    status: 400,
+                    headers: {
+                        'Content-Type': 'text/plain; charset=utf8'
+                    }
+                });
+            }
+
+
+        }
+        // return new Response("Hello")
+        return new Response('未能匹配到转义URL', {
+            status: 400,
+            headers: {
+                'Content-Type': 'text/plain; charset=utf8'
+            }
+        });
+
+    }
+
 }
